995dc67ba5433eff75c14355aa9611118d048736
alfiearmadillo <56178777+alfiearmadillo@users.noreply.github.com>
2022-06-21 16:23:25 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,3 @@
+function addClipboard() {
+    navigator.clipboard.writeText("javascript:(function(){document.body.appendChild(document.createElement('script')).src='https://alfiearmadillo.github.io/gatherioClient.js';})();");
+  }
